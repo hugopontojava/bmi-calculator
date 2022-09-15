c681abfcb43c8dd32335c804658adbcb68719848
djbetamax <hugots23@gmail.com>
2022-09-15 16:40:30 -0300

    feat: on click event

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-let kg = 0
-let alt = 0
-let imcCalc = (kg, alt) => (kg / (alt*2))
+let info = {
+  kg: '0',
+  height: '0',
+  imcCalc: (kg, alt) => { 
+    (kg / (alt*2))
+  }
+}
+
+const weight = document.querySelector('.weight');
+const height = document.querySelector('.height');
+
+
+const btn = document.getElementById('calc-btn') 
+btn.addEventListener('click', () => {
+  const resultBox = document.querySelector('.result');
+  const result = document.createTextNode(`Your BMI are:`)
+
+  resultBox.appendChild(result)
+})
